2fe0a9e30972f2303699fe66a42db02e479c902c
Renato Nouman <renato.nouman@gmail.com>
2020-08-04 23:36:29 -0300

    Refactor grid creation logic

--- src/App.js
+++ src/App.js
@@ -2,6 +2,50 @@ import React from "react";
 
 import { Bulb, Button, Container, Grid } from "./components";
 
+// base ensemble
+// -------------
+//
+// 100 lights
+// each light can be in one of 2 states: on / off
+// any light which is on has a 50/50 chance of going off in the next second
+// the system reach equilibrium and stops once all lights are off.
+// how long would it take for the system to reach equilibrium?
+//
+// first scenario
+// Connections are contructed so any light which is off has a 50/50 chance to go on in the next second, provided it is connected to a light the is on
+// If the light is not connected to a light that is on it has no change of going on.
+// If ever happens that al lights are off, then all lights stay off forever.
+//
+//
+// second scenario
+// there are no interconnections between lights.
+// when a light goes off, it stays off forever.
+//
+//
+// third scenario
+// Lights can turn on or off as long as there is a single light on.
+//
+
+// 100 lightbulbs
+//
+
+const line = (lineIndex) =>
+  [...Array(10)].reduce((line, _, index) => {
+    line.push({
+      id: `${lineIndex}${index}`,
+      coordinates: [lineIndex, index],
+      state: null,
+    });
+    return line;
+  }, []);
+
+const grid = [...Array(10)].reduce((grid, _, index) => {
+  grid.push(line(index));
+  return grid;
+}, []);
+
+console.log(grid);
+
 const App = () => {
   const [scenario, setScenario] = React.useState("connected");
 
@@ -30,11 +74,7 @@ const App = () => {
           Random
         </Button>
       </Button.Container>
-      <Grid
-        children={[...Array(100)].map((_, i) => (
-          <Bulb id={i} scenario={scenario} />
-        ))}
-      />
+      <Grid>{grid.map((line) => line.map((bulb) => <Bulb {...bulb} />))}</Grid>
     </Container>
   );
 };
--- src/components/Bulb.js
+++ src/components/Bulb.js
@@ -4,86 +4,86 @@ import { useMachine } from "@xstate/react";
 import { Machine } from "xstate";
 
 const BulbElement = styled.div`
-  border: 1px solid ${({ state }) => (state === "lit" ? "white" : "black")};
-  background-color: ${({ state }) => (state === "lit" ? "black" : "white")};
+  border: 1px solid ${({ state }) => (state === "on" ? "white" : "black")};
+  background-color: ${({ state }) => (state === "on" ? "black" : "white")};
   width: 36px;
   height: 36px;
   border-radius: 50%;
   display: inline-flex;
 `;
-const bulbMachine = Machine({
-  id: "bulb",
-  initial: "unlit",
-  states: {
-    lit: {
-      on: {
-        TOGGLE: {
-          target: "unlit",
-        },
-      },
-    },
-    unlit: {
-      on: {
-        TOGGLE: {
-          target: "lit",
-        },
-      },
-    },
-  },
-});
+//const bulbMachine = Machine({
+//  id: "bulb",
+//  initial: "unlit",
+//  states: {
+//    lit: {
+//      on: {
+//        TOGGLE: {
+//          target: "unlit",
+//        },
+//      },
+//    },
+//    unlit: {
+//      on: {
+//        TOGGLE: {
+//          target: "lit",
+//        },
+//      },
+//    },
+//  },
+//});
 
 const Bulb = (props) => {
-  const [state, send] = useMachine(bulbMachine);
+  //  const [state, send] = useMachine(bulbMachine);
 
-  const getSibling = (id, magicNum) => {
-    const sibling = document.getElementById((id + magicNum).toString());
-    return sibling && sibling.dataset.state;
-  };
+  console.log(props);
 
-  const initialToggle = React.useCallback(() => {
-    Math.random() * 100 <= 10 && send("TOGGLE");
-  }, [send]);
+  //  const getSibling = (id, magicNum) => {
+  //    const sibling = document.getElementById((id + magicNum).toString());
+  //    return sibling && sibling.dataset.state;
+  //  };
+  //
+  //  const initialToggle = React.useCallback(() => {
+  //    Math.random() * 100 <= 10 && send("TOGGLE");
+  //  }, [send]);
+  //
+  //  const toggle = React.useCallback(
+  //    (id) => {
+  //      const chance = Math.random() * 100 <= 50;
 
-  const toggle = React.useCallback(
-    (id) => {
-      const chance = Math.random() * 100 <= 50;
+  //      const siblings = {
+  //        up: getSibling(id, -11),
+  //        right: getSibling(id, 1),
+  //        down: getSibling(id, 11),
+  //        left: getSibling(id, -1),
+  //      };
 
-      const siblings = {
-        up: getSibling(id, -11),
-        right: getSibling(id, 1),
-        down: getSibling(id, 11),
-        left: getSibling(id, -1),
-      };
+  //      const RANDOM_CHANCE = props.scenario === "random" && chance;
+  //      const DISCONNECTED_CHANCE =
+  //        props.scenario === "disconnected" && state.value !== "lit" && chance;
+  //      const CONNECTED_CHANCE =
+  //        props.scenario === "connected" &&
+  //        Object.values(siblings).includes("lit") &&
+  //        chance;
 
-      const RANDOM_CHANCE = props.scenario === "random" && chance;
-      const DISCONNECTED_CHANCE =
-        props.scenario === "disconnected" && state.value !== "lit" && chance;
-      const CONNECTED_CHANCE =
-        props.scenario === "connected" &&
-        Object.values(siblings).includes("lit") &&
-        chance;
+  //      if (RANDOM_CHANCE) {
+  //        send("TOGGLE");
+  //      } else if (DISCONNECTED_CHANCE) {
+  //        send("TOGGLE");
+  //      } else if (CONNECTED_CHANCE) {
+  //        send("TOGGLE");
+  //      }
+  //    },
+  //    [props.scenario, send, state.value]
+  //  );
 
-      if (RANDOM_CHANCE) {
-        send("TOGGLE");
-      } else if (DISCONNECTED_CHANCE) {
-        send("TOGGLE");
-      } else if (CONNECTED_CHANCE) {
-        send("TOGGLE");
-      }
-    },
-    [props.scenario, send, state.value]
-  );
-
-  React.useLayoutEffect(() => {
-    initialToggle();
-    const interval = setInterval(() => toggle(props.id), 1000);
-
-    return () => clearInterval(interval);
-  }, [initialToggle, props.id, props.scenario, send, state.value, toggle]);
-
-  return (
-    <BulbElement id={props.id} data-state={state.value} state={state.value} />
-  );
+  //  React.useLayoutEffect(() => {
+  //    initialToggle();
+  //    const interval = setInterval(() => toggle(props.id), 1000);
+  //
+  //    return () => clearInterval(interval);
+  //  }, [initialToggle, props.id, props.scenario, send, state.value, toggle]);
+  //
+  return <BulbElement id={props.id} state={props.state} />;
 };
 
 export default Bulb;
