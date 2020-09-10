4e842ffb406eb26af33e25efeae4b4460fde41ca
Renato <renato.nouman@gmail.com>
2020-09-10 12:19:14 -0300

    Add first structure of text block

--- src/App.js
+++ src/App.js
@@ -22,16 +22,42 @@ const App = () => {
 
   return (
     <Container>
-      <Button.Container>
-        {scenarios.map((each) => (
-          <Button
-            key={each}
-            onClick={() => handleClick(each)}
-            active={scenario === each}
-            children={each}
-          />
-        ))}
-      </Button.Container>
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <p style={{ maxWidth: "600px", marginBottom: "240px" }}>
+          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        </p>
+
+        <Button.Container>
+          {scenarios.map((each) => (
+            <Button
+              key={each}
+              onClick={() => handleClick(each)}
+              active={scenario === each}
+              children={each}
+            />
+          ))}
+        </Button.Container>
+      </div>
       <Grid>
         {grid.map((bulb) => (
           <Bulb {...bulb} />
--- src/components/Container.js
+++ src/components/Container.js
@@ -4,8 +4,10 @@ const Container = styled.div`
   height: 100vh;
   display: grid;
   grid-template-columns: 1fr 1fr;
+  grid-gap: 36px;
   justify-content: center;
   align-items: center;
+  padding: 36px;
 `;
 
 export default Container;
