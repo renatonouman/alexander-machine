eaaddc13ac27404b38fb983c07de21406086bac6
Renato <renato.nouman@gmail.com>
2020-09-11 15:58:29 -0300

    Add logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-import { Paragraph, Button } from ".";
+import logo from "../assets/am-logo.svg";
 import DESCRIPTION from "../constants/DESCRIPTION";
+import { Paragraph, Button } from ".";
 
 const HeaderWrapper = styled.div`
   width: 100%;
@@ -12,9 +13,14 @@ const HeaderWrapper = styled.div`
   justify-content: center;
 `;
 
+const Logo = styled.img.attrs({ src: logo, alt: "Alexander Machine logo" })`
+  width: 150px;
+`;
+
 const Header = ({ scenario, handleClick }) => (
   <HeaderWrapper>
-    <h1>alexander-machine.</h1>
+    <Logo />
+    <h3>alexander-machine.</h3>
     <Paragraph>{DESCRIPTION}</Paragraph>
     <Button.Container>
       <Button
